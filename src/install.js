07f58c7f3a0bf9c2beea42ffda541d34bc0493bc
Ejez <ejez@protonmail.ch>
2021-03-24 08:06:50 +0300

    install.js: Add compatibility checks.

--- src/install.js
+++ src/install.js
@@ -1,10 +1,11 @@
-/**
- * Quasar App Extension install script
- *
- * Docs: https://quasar.dev/app-extensions/development-guide/install-api
- * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/InstallAPI.js
- */
-
 module.exports = function (api) {
-  //
+  // Quasar compatibility check.
+  api.compatibleWith('quasar', '>=2.0.0-beta.10 <3.0.0')
+  api.compatibleWith('@quasar/app', '>=3.0.0-beta.9 <4.0.0')
+
+  // Require vue-i18n v9 to be installed.
+  api.compatibleWith('vue-i18n', '>=9.0.0 <10.0.0')
+
+  // Render the templates in the app.
+  // api.render('./templates', { prompts: api.prompts })
 }
