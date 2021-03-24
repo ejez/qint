da25b6fbe54970e71795f62dc6c20651a36f3814
Ejez <ejez@protonmail.ch>
2021-03-24 08:08:09 +0300

    index.js: Register boot file and add webpack alias

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-/**
- * Quasar App Extension index/runner script
- * (runs on each dev/build)
- *
- * Docs: https://quasar.dev/app-extensions/development-guide/index-api
- * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
- */
+const path = require('path')
 
 module.exports = function (api) {
-  //
+  // App configuration.
+  api.extendQuasarConf((conf) => {
+    // Register the extension boot file.
+    conf.boot.push('~src/extensions/qint/boot')
+
+    // Make sure the extension files get transpiled.
+    conf.build.transpileDependencies.push(/quasar-app-extension-qint[\\/]src/)
+  })
+
+  // Add a webpack resolve alias for the extension.
+  api.extendWebpack((cfg) => {
+    cfg.resolve.alias.qint = path.resolve(__dirname)
+  })
 }
